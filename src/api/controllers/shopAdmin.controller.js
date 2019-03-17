e5959e66b038872a7309f13f78e7e32c5fec2000
ahmed elnady <dev.elnady@gmail.com>
2019-03-17 05:52:19 +0200

    paginate shop-admins

--- src/api/controllers/shopAdmin.controller.js
+++ src/api/controllers/shopAdmin.controller.js
@@ -9,7 +9,8 @@ router.post('/signup', async (req, res, next) => {
     try { 
         
         const createdShopAdmin = await ShopAdmin.create({
-            gatewayIDFK: req.body.GWUserID
+            gatewayIDFK: req.body.GWUserID,
+            name: req.body.GWUserName
         });
 
         res.status(201).send(createdShopAdmin);
@@ -27,7 +28,8 @@ router.post('/signup', async (req, res, next) => {
 router.get('/:gatewayUserId', async (req, res, next) => {
     try {
         const shopAdminDetails = await ShopAdmin.findOne({ gatewayIDFK: req.params.gatewayUserId })
-        res.status(200).send(shopAdminDetails)
+        res.status(200).send(shopAdminDetails);
+
     } catch (err) {
 
     }
@@ -37,9 +39,24 @@ router.get('/:gatewayUserId', async (req, res, next) => {
 //////////////////////////////////////////////////////////
 
 // all these routes allowed only by super-admin role
+
+
 router.post('/:id/approve', authorize(), async (req, res, next) => {
     try {
-        console.log('$$$$$ msg from approve')
+
+        const adminId = req.params.id; //TODO: validate it first 
+        const admin = await ShopAdmin.findById(adminId);
+        if (!admin) return res.status(404).json({ msg: `There are no admin with this id ${adminId} !`});
+
+        await ShopAdmin.updateOne({_id: adminId},{
+            $set: {
+                approved: true
+            }
+        });
+
+        res.status(200).json({ msg: `New admin has been approved.`});
+    
+
 
     } catch(e) {
 
@@ -48,27 +65,106 @@ router.post('/:id/approve', authorize(), async (req, res, next) => {
 
 router.delete('/:id/approve', authorize(), async (req, res, next) => {
     try {
+        const adminId = req.params.id; //TODO: validate it first 
+        const admin = await ShopAdmin.findById(adminId);
+        if (!admin) return res.status(404).json({ msg: `There are no admin with this id ${adminId} !`});
 
+        await ShopAdmin.updateOne({_id: adminId},{
+            $set: {
+                approved: false
+            }
+        });
+
+        res.status(200).json({ msg: `admin request has been rejected.`});
     } catch(e) {
 
     }
 });
 
-router.patch('/:id/assign-shop', authorize(), async (req, res, next) => {
+// to update activated, shopId and permissions
+router.patch('/:id', authorize(), async (req, res, next)=> {
     try {
+        const adminId = req.params.id, //TODO: validate it first     
+            isActivated = req.body.activated,
+            shopId = req.body.shopId,
+            permissions = req.body.permissions;
 
+        // Todo: seperate logic in a service 
+        const admin = await ShopAdmin.findById(adminId);
+        if (!admin) return res.json({ msg: `There are no admin with this id ${adminId} !`});
+
+        if(isActivated && !shopId) return res.json({ msg: `you should assign a shop to an activated admin !`});
+        
+        let setObj = {
+            activated: isActivated,
+            permissions: permissions
+        };
+        
+        // the case that this shopId is already assigned to another shop-admin (ask Business)
+        if(shopId) {                
+            // make it async operations (ask: business)
+            await ShopAdmin.findOneAndUpdate({shopId: shopId}, {
+                $set: {
+                    shopId: null,
+                    activated: false
+                }
+            })
+    
+            setObj.shopId = shopId;
+        }
+
+        await ShopAdmin.updateOne({ _id: adminId}, {
+            $set: setObj
+        });
+
+        res.status(200).json({
+            msg: 'shop-admin is updated successfully.'
+        });
     } catch(e) {
 
     }
-});
+})
 
-router.patch('/:id/roles', authorize(), async (req, res, next) => {
-    try {
+// refactorrrrrrrrrrrrrrrrrr
+router.post('/', authorize(), async (req, res, next) => {
+    const rqtype = req.body.report || req.query.report,
+        page = +req.body.page || +req.query.page || 1,
+        limit = +req.query.limit || 10,
+        paginationOptions = { select: 'name activated'};
 
-    } catch(e) {
 
-    }
-});
+    let query = {},
+        offset = (page -1) * limit;
 
+    if(!rqtype) return res.status(422).send({ error: 'Posted data is not correct or incompleted.'});
+    // todo: seperate in a service function 
+    if(rqtype === 'ALL_APPROVED') {
+        query = { approved: true };
+        const count = await ShopAdmin.count(query)
+        if(count > offset) offset = 0;
+    } else if (rqtype === 'ALL_DIS_APPROVED') { // admin requests
+        query = { approved: false };
+        const count = await ShopAdmin.count(query)
+        if(count > offset) offset = 0;
+    } else if (rqtype === 'ACTIVATED') {
+        query = { activated: true };
+        const count = await ShopAdmin.count(query)
+        if(count > offset) offset = 0;
+    } else if (rqtype === 'DIS_ACTIVATED') {
+        query = { activated: false };
+        const count = await ShopAdmin.count(query)
+        if(count > offset) offset = 0;
+    };
+
+    paginationOptions.offset = offset;
+    paginationOptions.limit = limit;
+
+
+    const paginatedAdmins = await ShopAdmin.paginate(query, paginationOptions)
+    res.status(200).json({
+        success: true,
+        data: paginatedAdmins
+    })
+})
 
 module.exports = router;    
