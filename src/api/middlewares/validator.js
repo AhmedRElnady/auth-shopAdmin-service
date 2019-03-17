22f4c699fa917dc4d5fdb39e5ea0fac65e9952be
ahmed elnady <dev.elnady@gmail.com>
2019-03-17 05:52:32 +0200

    .

--- src/api/middlewares/validator.js
+++ src/api/middlewares/validator.js
@@ -5,7 +5,7 @@ function validate(opt) { /// clouser with opt
     return (req, res, next) => {
         (async ()=>{
             console.log(">>> msg from middle ware >>>", opt);
-
+            
             next()
         })()
     }
